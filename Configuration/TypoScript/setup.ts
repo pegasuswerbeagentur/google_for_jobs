e745658a9e8bbdd9c3e72d1674e327f89b2dc5df
Michael Kunst <kunst@pega-sus.de>
2019-09-20 09:33:28 +0200

    remove css default style, add setting for detail page rendering

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -22,38 +22,9 @@ plugin.tx_googleforjobs_job {
     mvc {
         #callDefaultActionIfActionCantBeResolved = 1
     }
+    settings {
+        job {
+            renderDetailTemplate = 1
+        }
+    }
 }
-
-# these classes are only used in auto-generated templates
-plugin.tx_googleforjobs._CSS_DEFAULT_STYLE (
-    textarea.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    input.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    .tx-google-for-jobs table {
-        border-collapse:separate;
-        border-spacing:10px;
-    }
-
-    .tx-google-for-jobs table th {
-        font-weight:bold;
-    }
-
-    .tx-google-for-jobs table td {
-        vertical-align:top;
-    }
-
-    .typo3-messages .message-error {
-        color:red;
-    }
-
-    .typo3-messages .message-ok {
-        color:green;
-    }
-)
