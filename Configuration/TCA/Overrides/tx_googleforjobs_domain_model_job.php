01f562c788d1abdd5385c55d088bb23c382fe4c1
Michael Kunst <kunst@pega-sus.de>
2021-03-09 11:00:55 +0100

    [FIX] move strict type declaration to top of file

--- Configuration/TCA/Overrides/tx_googleforjobs_domain_model_job.php
+++ Configuration/TCA/Overrides/tx_googleforjobs_domain_model_job.php
@@ -1,8 +1,8 @@
 <?php
 
-defined('TYPO3_MODE') or die();
-
 declare(strict_types=1);
+
+defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
     'google_for_jobs',
